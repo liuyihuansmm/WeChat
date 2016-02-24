1f95e9349ef5af7202eced0a5cf78c2e8dc13626
LYH <liuyihuansmm@163.com>
2016-02-24 14:04:02 +0800

    文本消息实体类

---
+++ src/com/scyy/po/TextMessage.java
@@ -0,0 +1,63 @@
+package com.scyy.po;
+
+/**
+ * Created by LYH on 2016/2/24.
+ *  说明：文本消息实体类
+ */
+public class TextMessage {
+
+    private  String ToUserName;
+    private  String FromUserName;
+    private  Long   CreateTime;
+    private  String MsgType;
+    private  String Content;
+    private  String MsgId;
+
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+
+    public Long getCreateTime() {
+        return CreateTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        CreateTime = createTime;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getMsgId() {
+        return MsgId;
+    }
+
+    public void setMsgId(String msgId) {
+        MsgId = msgId;
+    }
+}
